38732fa0342f0327a791bf31b3e9bbd7917e70d5
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-02-21 13:16:13 +0100

    Created console app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MosamaticWeb4
+
 New and simplified version of Mosamatic using Briefcase
+
+# Introduction
+
+Mosamatic Web 4 is the latest incarnation of the Mosamatic tool for body composition
+analysis with CT images at L3 level.
--- mosamaticweb4/src/mosamaticweb4/app.py
+++ mosamaticweb4/src/mosamaticweb4/app.py
@@ -8,14 +8,18 @@ def main():
     appPath = os.path.join(os.path.abspath(__file__))
     appPath = os.path.dirname(appPath)
     appPath = os.path.join(appPath, 'backend')
+    
     sys.path.append(appPath)
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     os.environ.setdefault('OAUTHLIB_INSECURE_TRANSPORT', '1')
+    
     os.chdir(appPath)
+    
     print('##############################################################################')
     print('#                   M O S A M A T I C   W E B  4                             #')
     print('##############################################################################')
-    print(appPath)
+    
     execute_from_command_line(['manage.py', 'makemigrations'])
     execute_from_command_line(['manage.py', 'migrate'])
     # execute_from_command_line(['manage.py', 'create_admin_user'])
